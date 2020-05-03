dfa1baeb8340b925d2fc7dd8e131cd585296baad
Veli Akiner <veli@kopernio.com>
2020-05-03 13:37:15 +0100

    Update description
    Noticed this when I saw the PyPi page

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     maintainer_email="veli@kopernio.com",
     license="MIT",
     url="https://github.com/kopernio/pytest-hoverfly-wrapper",
-    description="TBD",
+    description="Integrates the Hoverfly HTTP proxy into Pytest",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
