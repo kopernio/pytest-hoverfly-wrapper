6840e4e6e555bd4d6b4be96a6b4890d31f8fba50
Veli Akiner <veli@kopernio.com>
2020-06-11 19:28:59 +0100

    bump to 0.3.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pytest-hoverfly-wrapper",
-    version="0.3.0",
+    version="0.3.1",
     author="Veli Akiner",
     author_email="veli@kopernio.com",
     maintainer="Veli Akiner",
