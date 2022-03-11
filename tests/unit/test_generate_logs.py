aa3aa8c8d0a244ba1c6f6d330f6b56fe1ec8b121
Veli Akiner <veli.akiner@gmail.com>
2022-03-11 20:58:31 +0000

    test_generate_logs: split into individual tests

--- tests/unit/test_generate_logs.py
+++ tests/unit/test_generate_logs.py
@@ -7,23 +7,43 @@ import requests
 from pytest_hoverfly_wrapper.plugin import generate_logs
 
 
-def test_generate_logs(mocker, tmpdir):
-    mock_request = mocker.MagicMock()
-    mock_request.node.sensitive = ["sensitive.host"]
-    mock_request.node.mode = "simulate"
-    mock_journal_api = mocker.MagicMock()
+@pytest.fixture
+def mock_journal_api(mocker):
+    return mocker.MagicMock()
+
+
+@pytest.fixture
+def mock_request(mocker):
+    mock_request_ = mocker.MagicMock()
+    mock_request_.node.sensitive = ["sensitive.host"]
+    mock_request_.node.mode = "simulate"
+    return mock_request_
+
+
+@pytest.fixture
+def log_file(tmpdir):
+    return os.path.join(tmpdir.strpath, "network.json")
+
+
+def test_generate_logs(mock_request, tmpdir, mock_journal_api, log_file):
+    """Golden path"""
     with open("tests/input.json") as f:
         mock_journal_api.get.return_value = json.load(f)
-    log_file = os.path.join(tmpdir.strpath, "network.json")
-    # golden path
     generate_logs(request=mock_request, journal_api=mock_journal_api, test_log_directory=tmpdir.strpath)
     assert os.path.isfile(log_file)
-    # exception raised if sensitive host isn't cached
+
+
+def test_sensitive_hosts_checked(tmpdir, mock_request, mock_journal_api):
+    """Exception raised if sensitive host isn't cached"""
+    with open("tests/input.json") as f:
+        mock_journal_api.get.return_value = json.load(f)
     del mock_journal_api.get.return_value["journal"][0]["response"]["headers"]["Hoverfly-Cache-Served"]
     with pytest.raises(AssertionError):
         generate_logs(request=mock_request, journal_api=mock_journal_api, test_log_directory=tmpdir.strpath)
 
-    # useful message dumped if hoverfly crashes during log retrieval
+
+def test_log_crash(tmpdir, log_file, mock_request, mock_journal_api):
+    """Useful message dumped if hoverfly crashes during log retrieval"""
     mock_journal_api.get.side_effect = requests.exceptions.ConnectionError
     generate_logs(request=mock_request, journal_api=mock_journal_api, test_log_directory=tmpdir.strpath)
     with open(log_file) as f:
