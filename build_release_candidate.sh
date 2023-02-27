14033f3f6501f47793a5cdc95f258ddcedfeb12f
Veli Akiner <veli.akiner@gmail.com>
2023-02-27 22:27:24 +0000

    Build a release candidate if repo version is ahead of deployed

---
+++ build_release_candidate.sh
@@ -0,0 +1,11 @@
+# Checks if there has been a new version change, and builds the plugin for release if there has.
+pip install yolk3k
+LATEST_DEPLOYED_VER=$(yolk -V pytest-hoverfly-wrapper | grep -oE '[0-9.]+')
+THIS_VER=$(python setup.py -V)
+if [[ "$THIS_VER" > "$LATEST_DEPLOYED_VER" ]];then
+  echo "Version bumped from $LATEST_DEPLOYED_VER to $THIS_VER. Building"
+  pip install build
+  python -m build
+else
+  echo "Version not bumped. Not building."
+fi
